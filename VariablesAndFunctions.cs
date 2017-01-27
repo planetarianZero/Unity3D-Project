750d3b4f89c1dc7e3921c7924266863259e73aa4
planetarianZero <m18333586810@163.com>
2017-01-27 18:27:49 +0800

    Create VariablesAndFunctions.cs

---
+++ VariablesAndFunctions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour 
+{
+    int myInt = 5;
+	// Use this for initialization
+	void Start () 
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+	}
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
